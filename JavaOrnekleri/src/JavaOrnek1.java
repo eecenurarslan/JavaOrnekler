70a01dc4a1253f757b4835d8ad936067da8bbb27
Ece <aecenur385@gmail.com>
2022-12-08 20:58:26 +0300

    simple code
    oryante ödev örnekleri

---
+++ JavaOrnekleri/src/JavaOrnek1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author asus
+ *
+ */
+public class JavaOrnek1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
